ba2bee02418d57e85bce6febe9d327d39735acdb
Akbar Satrio Nugroho <Akbar Satrio Nugroho>
2023-02-04 20:34:16 +0700

    Melakukan penyesuaian dalam option generate center pada kemunculan aplikasi

--- src/todolist/Login.java
+++ src/todolist/Login.java
@@ -375,6 +375,7 @@ public class Login extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void usernameTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_usernameTextFieldActionPerformed
